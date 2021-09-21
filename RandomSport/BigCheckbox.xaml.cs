3e89a99b4cb4808f3f6b362a20bff1d3aca23f2e
Vladimir Kravchenko <56361236+vlad-kravchenko@users.noreply.github.com>
2021-09-21 14:06:53 +0300

    Changed Checkbox size, made custom control

---
+++ RandomSport/BigCheckbox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomSport
+{
+    /// <summary>
+    /// Логика взаимодействия для BigCheckbox.xaml
+    /// </summary>
+    public partial class BigCheckbox : UserControl
+    {
+        public BigCheckbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
